d1ed1b6e0e782eb70091a7e045f590bb0d79929b
Kylie <kyliecalif@gmail.com>
2018-03-25 19:14:42 -0700

    menu bar needs cssand js for the popups/dropdowns. weather cards and styling completed.

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 $(document)
     .ready(function() {
-     
-      
-});
-    
+
+        var today = new Date();
+
+        // $('#datePicker').calendar({
+        //     on: 'hover'
+        // });
+
+
+        // $('#example1').calendar();
+
+        // $('.ui.calendar').calendar({
+        //     onChange: function(date, text) {
+        //         var newValue = text;
+        //         alert(newValue);
+        //     },
+        // });
+
+        $('.ui.icon.button').popup({
+            on: 'hover',
+            // hoverable:false,
+            title: "Number of Sunny Days",
+            content: "xx"
+        });
+
+        // $('.ui.popup').popup({
+        //    		title   : 'Number of Sunny Days',
+        //    		content : '47 Sunny Daze'
+        //  	});
+    });
